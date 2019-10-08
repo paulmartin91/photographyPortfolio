857fcf49cc5b39b5a8c99b14b5fb8950bcd25060
Paul Martin <paul.martin91@hotmail.co.uk>
2019-10-08 16:19:52 +0100

    Updates

---
+++ precache-manifest.651d6ac3b0325c15865dac4cce7ae2bc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac60d532a2987d01ea9f91e65bff34b8",
+    "url": "/photographyPortfolio/index.html"
+  },
+  {
+    "revision": "003426818979c77629f7",
+    "url": "/photographyPortfolio/static/css/main.61d0f9b9.chunk.css"
+  },
+  {
+    "revision": "7632f2226f7f4c8e783d",
+    "url": "/photographyPortfolio/static/js/2.fa1a19a0.chunk.js"
+  },
+  {
+    "revision": "003426818979c77629f7",
+    "url": "/photographyPortfolio/static/js/main.9d206b59.chunk.js"
+  },
+  {
+    "revision": "53ab17c898c34a983f60",
+    "url": "/photographyPortfolio/static/js/runtime-main.8cce023d.js"
+  },
+  {
+    "revision": "289f16e3718cf93559863528e4fb4fdd",
+    "url": "/photographyPortfolio/static/media/BG1.289f16e3.png"
+  },
+  {
+    "revision": "58a81b9eaa11a9220066686ca89e38dc",
+    "url": "/photographyPortfolio/static/media/BG2.58a81b9e.png"
+  },
+  {
+    "revision": "9c6ca8f5f794c4ae9c530fa7e7ce33dd",
+    "url": "/photographyPortfolio/static/media/BG3.9c6ca8f5.png"
+  },
+  {
+    "revision": "8c397ce42e5ce824d7e40fea04250051",
+    "url": "/photographyPortfolio/static/media/BG4.8c397ce4.png"
+  },
+  {
+    "revision": "673ac8da042f313772fa05439e905758",
+    "url": "/photographyPortfolio/static/media/BG5.673ac8da.png"
+  },
+  {
+    "revision": "69a8014430e8bec88c10383ddc2dc6b8",
+    "url": "/photographyPortfolio/static/media/BG6.69a80144.png"
+  }
+]);
