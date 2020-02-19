67b120b75542674e90b09f24e7dce228e3203068
Paul Martin <paul.martin91@hotmail.co.uk>
2020-02-19 22:45:04 +0000

    smaller photo file sizes and streamlined file structure

--- src/App.js
+++ src/App.js
@@ -1,38 +1,104 @@
 import React from 'react';
 import Photos from './Photos'
+import AlbumMenu from './AlbumMenu'
 import './App.css';
-import BG1 from './PhotosMain/BG1.png'
-import BG2 from './PhotosMain/BG2.png'
-import BG3 from './PhotosMain/BG3.png'
-import BG4 from './PhotosMain/BG4.png'
-import BG5 from './PhotosMain/BG5.png'
-import BG6 from './PhotosMain/BG6.png'
 
 class App extends React.Component{
   constructor(props){
     super(props)
+    this.state = {
+      albumOpen: false,
+      albumFilter: "All",
+      kitShow: false,
+    }
   }
+
+  handleClick = (event) => {
+    if (event.target.name == "album") {
+      this.setState(prevState => ({
+        albumOpen: !prevState.albumOpen,
+        kitShow: false
+    }));
+    }
+
+    if (event.target.name == "Kit") {
+        // document.getElementsByClassName("photoMain")[0].src = KIT
+        this.setState(prevState => ({
+          kitShow: !prevState.kitShow,
+          albumOpen: false
+      }))
+    }
+
+    if (event.target.name == "albumFilter") {
+      var nme = event.target.innerHTML
+      console.log(nme)
+      if (this.state.albumFilter != event.target.innerHTML) {
+        let x = document.getElementsByClassName('photoMain')
+        //fade existing photos
+          for (let i = 0; i<x.length; i++) {
+            // setTimeout(()=>x[i].style.opacity = 0, i*100)
+            x[i].style.opacity = 0;
+          }
+      
+          setTimeout(()=>{
+            document.getElementById('photoMainBox').scrollLeft = 0 //this one
+            this.setState({
+              albumFilter: [nme]
+            })
+          }, 300)
+
+      setTimeout(()=>{
+        let x = document.getElementsByClassName('photoMain')
+        for (let i = 0; i<x.length; i++) {
+          // setTimeout(()=>x[i].style.opacity = 0, i*100)
+          x[i].style.opacity = 1;
+        }
+      }, 350, nme)
+      }
+    }
+  }
+
   render(){
+
     return(
       <section>
         <div className="app">
           <div className = "mainMenu">
           <div className="logo">LOGO</div>
             <div className="menuItems">
-              <a className="menuItem">Home</a>
-              <a className="menuItem">Albums</a>
-              <a className="menuItem">About</a>
-              <a className="menuItem">Contact</a>
+              <a
+                name = "album"
+                className="menuItem"
+                className={(this.state.albumOpen) ? "unselected" : "menuHover"}
+                onClick={this.handleClick}>
+                  Albums
+              </a>
+              <a
+                name = "Kit"
+                className="menuItem"
+                className={(this.state.kitShow) ? "unselected" : "menuHover"}
+                onClick={this.handleClick}
+                >
+                  Kit
+              </a>
+              <a
+                name = "About"
+                className="menuItem menuHover"
+                // className={(this.state.albumOpen) ? "unselected" : "menuHover"} 
+                >
+                  About
+              </a>
+              <a
+                name = "Contact"
+                className="menuItem menuHover"
+                // className={(this.state.albumOpen) ? "unselected" : "menuHover"} 
+                >
+                  Contact
+              </a>
             </div>
           </div>
-          <div className="photosMainBox">
-            <Photos photo={BG1}/>
-            <Photos photo={BG2}/>
-            <Photos photo={BG3}/>
-            <Photos photo={BG4}/>
-            <Photos photo={BG5}/>
-            <Photos photo={BG6}/>
-          </div>
+              <AlbumMenu albumOpen={this.state.albumOpen} handleClick={this.handleClick}/>
+              <Photos albumOpen = {this.state.albumOpen} albumFilter={this.state.albumFilter} kitShow={this.state.kitShow}/>
         </div>
       </section>
     )
--- src/Photos.js
+++ src/Photos.js
@@ -1,11 +1,44 @@
 import React from 'react';
 import './App.css';
+import BG1 from './PhotosMain/BG1.jpg'
+import BG2 from './PhotosMain/BG2.jpg'
+import BG3 from './PhotosMain/BG3.jpg'
+import BG4 from './PhotosMain/BG4.jpg'
+import BG5 from './PhotosMain/BG5.jpg'
+import BG6 from './PhotosMain/BG6.jpg'
+import BG7 from './PhotosMain/BG7.jpg'
+import BG8 from './PhotosMain/BG8.jpg'
+import BG9 from './PhotosMain/BG9.jpg'
+import BG10 from './PhotosMain/BG10.jpg'
+
+import KitPannel from './KitPannel'
 
 const Photos = (props) => {
+
+    const photos = {
+        All: [BG1, BG2, BG3, BG4, BG5, BG6, BG7, BG8, BG9, BG10],
+        Jungle: [BG1, BG2, BG3],
+        Desert: [BG4, BG5],
+        England: [BG6, BG7, BG8],
+        Tundra: [BG9, BG10]
+      }
+
     return (
-        <div>
-            <img className="photoMain" src={props.photo} />
+        // <section id="albumFilter" className="photosMainBox" >
+        // {photos[props.albumFilter].map((x, y)=>
+        //         (y==0 && props.kitShow) ? 
+        //             <CrossfadeImage src={KIT} className="photoMain"/> 
+        //             : 
+        //             <CrossfadeImage src={x} className="photoMain"/>
+        //         )}
+        // </ section>
+        <div className="photosMainBox" id='photoMainBox'>
+        <KitPannel albumOpen = {props.albumOpen} kitShow={props.kitShow} />
+ {photos[props.albumFilter].map((x, y)=>
+            <img src={x} class = "photoMain"/>
+            )}
         </div>
+        
     )
 }
 
