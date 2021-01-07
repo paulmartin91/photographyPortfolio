eeb7a5bc828ee32d98b99fb34ce605c842ac573b
Paul Martin <paul.martin91@hotmail.co.uk>
2021-01-07 18:45:01 +0000

    with contact and about

--- src/About.js
+++ src/About.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import FadeIn from 'react-fade-in';
 
+import Me from "./PhotosMain/me.jpg"
+
 import './App.css';
 
 const About = (props) => {
+    const containerStyle = {
+        maxWidth: 300,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "flex-end",
+        paddingBottom: 50,
+        fontFamily: "Playfair Display"
+    }
     return (
         <div 
         className="aboutBox"
-        style = {(props.aboutShow && !props.albumOpen) ? {minWidth: "510px"} : {minWidth: "0"}}
+        style = {(props.aboutShow && !props.albumOpen) ? {minWidth: "400px"} : {minWidth: "0"}}
         >
-            {(props.aboutShow) && <FadeIn delay={200}> <p>Hello World!</p> </FadeIn>}
+            {(props.aboutShow) &&
+                <div style={containerStyle}>
+                <FadeIn delay={300}>
+                    <h1 >About me</h1> 
+                    <p >Thanks for stopping by! I'm a software developer who likes to take pictures in his spare time. I take my camera along whenever I'm lucky enough to travel anywhere and I take photos of people or things I find interesting. Feel free to have a look around and please enjoy my photos!</p>
+                    <img style={{width: 300}} src={Me}/>
+                </FadeIn>
+                </div>
+            }
         </div>
     )
 }
--- src/Contact.js
+++ src/Contact.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import FadeIn from 'react-fade-in';
 
+
 import './App.css';
+
+//logos
+import FCC from './PhotosMain/FCC.png'
+import Github from './PhotosMain/Github.png'
+import CodeWars from './PhotosMain/CodeWars.png'
+import LinkedIn from './PhotosMain/LinkedIn.png'
 
 const Contact = (props) => {
     return (
@@ -9,7 +16,19 @@ const Contact = (props) => {
         className="contactBox"
         style = {(props.contactShow && !props.albumOpen) ? {minWidth: "510px"} : {minWidth: "0"}}
         >
-            {(props.contactShow) && <FadeIn delay={200}> <p>Hello World!</p> </FadeIn>}
+            {(props.contactShow) && 
+            <FadeIn delay={350}>
+                <section className='contactHolder'>
+                    <p>Say Hi <span role="img">👋</span></p>
+                        <p2>Email: <a href="mailto:pauledwardmartin91@gmail.com" >pauledwardmartin91@gmail.com</a></p2>
+                    <div className = "socialAll">
+                         <FadeIn delay={350}> <a href="https://www.freecodecamp.org/paulmartin" target="_blank"><img src={FCC} className="socialImages" alt="Free Code Camp"/></a>   </FadeIn>
+                         <FadeIn delay={400}> <a href="https://github.com/paulmartin91/" target="_blank"><img src={Github} className="socialImages" alt="Github"/></a>  </FadeIn>
+                         <FadeIn delay={450}> <a href="https://www.codewars.com/users/paulimo/completed" target="_blank"><img src={CodeWars} className="socialImages" alt="Codewars"/></a> </FadeIn>
+                         <FadeIn delay={500}> <a href="https://www.linkedin.com/in/paul-martin-35b24569/" target="_blank"><img src={LinkedIn} className="socialImages" alt="LinkedIn"/></a> </FadeIn>
+                    </div> 
+                </ section>
+            </FadeIn>}
         </div>
     )
 }
